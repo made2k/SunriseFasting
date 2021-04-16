95f623b670df5d7a676600d6edc3d45c85d6702e
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2021-04-16 13:54:23 -0600

    Initial Commit

---
+++ Fasting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fasting
+//
+//  Created by Zach McGaughey on 4/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
