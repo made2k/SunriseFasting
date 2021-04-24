5a203d00a7eded1ce85e6580344bc0ba62ccacd3
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2021-04-23 23:52:21 -0600

    Add files not added by xcode

---
+++ Fasting/FastingData/Models/FastGroup/FastGroup+Identifiable.swift
@@ -0,0 +1,14 @@
+//
+//  FastGroup+Identifiable.swift
+//  Fasting
+//
+//  Created by Zach McGaughey on 4/23/21.
+//
+
+import Foundation
+
+extension FastGroup: Identifiable {
+  
+  var id: String { title }
+  
+}
