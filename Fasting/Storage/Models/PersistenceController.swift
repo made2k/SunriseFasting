1baed972467230af7cc6b0d103de0c9b78ed630c
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2021-09-27 16:18:20 -0600

    Database will move to app group directory

--- Fasting/Storage/Models/PersistenceController.swift
+++ Fasting/Storage/Models/PersistenceController.swift
@@ -9,9 +9,9 @@ import CoreData
 import OSLog
 
 struct PersistenceController {
-
+  
   static let logger = Logger.create(.coreData)
-
+  
   // MARK: - Static Accessors
   
   /// The shared controller that will persist data to disk.
@@ -20,7 +20,7 @@ struct PersistenceController {
   /// In memory store that does not write any data to disk
   static var preview: PersistenceController = {
     let result = PersistenceController(inMemory: true)
-
+    
     // Backfill history data
     for value in 2..<34 {
       let entity = Fast(context: result.container.viewContext)
@@ -46,21 +46,55 @@ struct PersistenceController {
     
   }()
   
+  private static func appGroupFileURL() -> URL {
+    let appGroupDirectoryUrl: URL = FileManager.default
+      .containerURL(forSecurityApplicationGroupIdentifier: "group.com.zachmcgaughey.Fasting.data")
+      .unsafelyUnwrapped
+    let dataUrl: URL = appGroupDirectoryUrl.appendingPathComponent("Fasting.sqlite")
+    return dataUrl
+  }
+  
+  private static func applicationURL() -> URL {
+    let supportDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+    let dataUrl = supportDirectory.appendingPathComponent("Fasting.sqlite")
+    return dataUrl
+  }
+  
+  
   // MARK: - Properties
   
   let container: NSPersistentContainer
   
   // MARK: - Lifecycle
-
+  
   private init(inMemory: Bool = false) {
     self.container = NSPersistentContainer(name: "Fasting")
     
     if inMemory {
       Self.logger.trace("Created memory persistent container")
       container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+      
+    } else {
+      let appGroupUrl = Self.appGroupFileURL()
+      let appGroupPath = appGroupUrl.absoluteString.replacingOccurrences(of: "file://", with: "").removingPercentEncoding.unsafelyUnwrapped
+      let newFileExists: Bool = FileManager.default.fileExists(atPath: appGroupPath)
+      
+      let supportUrl = Self.applicationURL()
+      let supportPath = supportUrl.absoluteString.replacingOccurrences(of: "file://", with: "").removingPercentEncoding.unsafelyUnwrapped
+      let oldFileExists: Bool = FileManager.default.fileExists(atPath: supportPath)
+      
+      if oldFileExists && newFileExists == false {
+        migrateToAppGroup(container: container, supportURL: supportUrl, appGroupURL: appGroupUrl)
+        return
+      }
+      
+      let description = NSPersistentStoreDescription(url: appGroupUrl)
+      container.persistentStoreDescriptions = [description]
     }
     
-    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+    let semephore = DispatchSemaphore(value: 1)
+    
+    container.loadPersistentStores { storeDescription, error in
       if let error = error as NSError? {
         Self.logger.error("Failed to load persistent stores: \(error.localizedDescription)")
         // Replace this implementation with code to handle the error appropriately.
@@ -78,6 +112,38 @@ struct PersistenceController {
       } else {
         Self.logger.debug("Persistent stores loaded: \(storeDescription)")
       }
-    })
+      
+      semephore.signal()
+    }
+    
+    semephore.wait()
   }
+  
+  private func migrateToAppGroup(container: NSPersistentContainer, supportURL: URL, appGroupURL: URL) {
+    let coordinator = container.persistentStoreCoordinator
+    
+    let oldStoreDescription = NSPersistentStoreDescription(url: supportURL)
+    container.persistentStoreDescriptions = [oldStoreDescription]
+    
+    let semephore = DispatchSemaphore(value: 1)
+    
+    container.loadPersistentStores { storeDescription, error in
+      guard let url = storeDescription.url else { return }
+      guard let oldStore = coordinator.persistentStore(for: url) else { return }
+      
+      do {
+        try coordinator.migratePersistentStore(oldStore, to: appGroupURL, options: nil, withType: NSSQLiteStoreType)
+        try coordinator.destroyPersistentStore(at: url, ofType: NSSQLiteStoreType, options: nil)
+        
+      } catch {
+        print("Error migrating store: \(error.localizedDescription)")
+      }
+      
+      semephore.signal()
+      
+    }
+    
+    semephore.wait()
+  }
+  
 }
