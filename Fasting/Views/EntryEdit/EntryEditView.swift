ad86161c4abdafb6a5d6d2832cbb141b62fd067b
Zach McGaughey <1799888+made2k@users.noreply.github.com>
2022-09-19 20:17:34 -0600

    Better UI for entry edit

--- Fasting/Views/EntryEdit/EntryEditView.swift
+++ Fasting/Views/EntryEdit/EntryEditView.swift
@@ -15,6 +15,7 @@ struct EntryEditView: View {
   @ObservedObject var model: FastModel
   @State var presented: AnyView?
   @ObservedObject var keyboardHeightHelper = KeyboardHeightHelper()
+  @Environment(\.dismiss) private var dismiss
   
   var buttonColor: Color {
     model.entity.progress < 1 ?
@@ -30,75 +31,114 @@ struct EntryEditView: View {
   
   var body: some View {
     
-    ZStack {
+    NavigationView {
       
-      ScrollViewReader { reader in
-        ScrollView {
-          
-          VStack {
+      ZStack {
+        
+        Color.secondarySystemBackground
+          .ignoresSafeArea(.all)
+        
+        ScrollViewReader { reader in
+          ScrollView {
             
-            ZStack {
-              RingView(ConstantUpdater(model.entity.progress))
-                .applyProgressiveStyle(model.entity.progress)
-                .thickness(28)
-                .aspectRatio(contentMode: .fit)
+            VStack {
               
-              VStack {
-                Text("Fasting Duration (\(model.progress.formatted(.percentRounded)))")
-                  .monospaced(font: .footnote)
-                  .foregroundColor(Color(UIColor.secondaryLabel))
-                Text(model.entity.currentInterval.formatted(.shortDuration))
-                  .monospaced(font: .largeTitle)
+              ZStack {
+                RingView(ConstantUpdater(model.entity.progress))
+                  .applyProgressiveStyle(model.entity.progress)
+                  .thickness(28)
+                  .aspectRatio(contentMode: .fit)
+                
+                VStack {
+                  Text("Fasting Duration (\(model.progress.formatted(.percentRounded)))")
+                    .monospaced(font: .footnote)
+                    .foregroundColor(Color(UIColor.secondaryLabel))
+                  Text(model.entity.currentInterval.formatted(.shortDuration))
+                    .monospaced(font: .largeTitle)
+                }
               }
-            }
-            .frame(maxHeight: 350)
-            
-            HStack {
-              Button(model.startDate.formatted(date: .omitted, time: .shortened)) {
-                editStartTime()
+              .id(0)
+              .frame(maxHeight: 350)
+              
+              HStack {
+                Button(model.startDate.formatted(date: .omitted, time: .shortened)) {
+                  editStartTime()
+                }
+                Text(" - ")
+                Button(model.endDate?.formatted(date: .omitted, time: .shortened) ?? "?") {
+                  editEndTime()
+                }
               }
-              Text(" - ")
-              Button(model.endDate?.formatted(date: .omitted, time: .shortened) ?? "?") {
-                editEndTime()
+              .buttonStyle(PaddedButtonStyle(foregroundColor: buttonColor))
+              
+              HStack {
+                Spacer()
+                MoodButton(mood: $model.mood, tintColor: model.entity.progress < 1 ? Color.buttonForegroundIncomplete : Color.buttonForegroundComplete)
+                  .padding(.trailing, 24)
               }
+              
+              VStack(alignment: .leading) {
+                Text("Enter notes about your fast")
+                  .font(.callout)
+                  .foregroundColor(.secondaryLabel)
+                  .padding([.leading], 8)
+                TextEditor(text: $model.note ?? "")
+                  .frame(height: 200)
+                  .cornerRadius(14)
+                  .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                      Spacer()
+                      Button(action: {
+                        UIApplication.shared.sendAction(
+                          #selector(UIResponder.resignFirstResponder),
+                          to: nil,
+                          from: nil,
+                          for: nil
+                        )
+                      }, label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                      })
+                    }
+                  }
+                  .overlay(
+                    RoundedRectangle(cornerRadius: 14)
+                      .stroke(Color.secondaryLabel, lineWidth: 2)
+                  )
+              }
+              .padding([.leading, .trailing], 24)
+              .id(1)
+              
             }
-            .buttonStyle(PaddedButtonStyle(foregroundColor: buttonColor))
-            
-            HStack {
-              Spacer()
-              MoodButton(mood: $model.mood, tintColor: model.entity.progress < 1 ? Color.buttonForegroundIncomplete : Color.buttonForegroundComplete)
-                .padding(.trailing, 24)
+          }
+          .keyboardDismissal()
+          .onChange(of: keyboardHeightHelper.keyboardHeight) { _ in
+            withAnimation {
+              if keyboardHeightHelper.keyboardHeight > 0 {
+                reader.scrollTo(1)
+                
+              } else {
+                reader.scrollTo(0)
+              }
+              
             }
-            
-            VStack(alignment: .leading) {
-              Text("Enter notes about your fast")
-                .font(.callout)
-                .foregroundColor(.secondaryLabel)
-                .padding([.leading], 8)
-              TextEditor(text: $model.note ?? "")
-                .frame(height: 200)
-                .cornerRadius(14)
-                .overlay(
-                  RoundedRectangle(cornerRadius: 14)
-                    .stroke(Color.secondaryLabel, lineWidth: 2)
-                )
-            }
-            .padding([.leading, .trailing], 24)
-            .id(1)
-
           }
         }
-        .background(Color.secondarySystemBackground)
-        .onChange(of: keyboardHeightHelper.keyboardHeight) { _ in
-          withAnimation {
-            reader.scrollTo(1)
-          }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle(model.startDate.formatted(date: .abbreviated, time: .omitted))
+        .toolbar {
+          Button(action:{
+            dismiss()
+          }, label: {
+            Image(systemName: "xmark.circle.fill")
+          })
         }
-      }
-      
-      if let presented = presented {
-        presented
-          .zIndex(2)
+        .tint(.secondaryLabel)
+        
+        
+        if let presented = presented {
+          presented
+            .zIndex(2)
+        }
       }
       
     }
